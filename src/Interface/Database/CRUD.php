918fd165db053572111054dca29558d394f25ce0
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-04-17 02:51:55 +0800

    create Script test

--- src/Interface/Database/CRUD.php
+++ src/Interface/Database/CRUD.php
@@ -7,7 +7,7 @@ interface CRUD {
 
     /**
      * @param $Connector PDO
-     * @param $table_name QueryString
+     * @param $table_name String
      */
     public static function Read($Connector, $table_name) ;
 }
