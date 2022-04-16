d84fa5ccd18bfa57a89b1f8735047f8507d4dd70
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-04-17 02:51:22 +0800

    create Script test

--- src/Module/Database/CRUD.php
+++ src/Module/Database/CRUD.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace yovanggaanandhika\dkaframework\Module\Database;
+
+use PDO;
 use yovanggaanandhika\dkaframework\Interface\Database\CRUD as CRUDInterface;
 
-class CRUD implements CRUDInterface {
+class CRUD extends PDO implements CRUDInterface {
 
     public static function Read($Connector, $table_name)
     {
         $reformatTableName = strval($table_name);
-        $statement = $Connector->prepare("SELECT :array FROM :table_name");
+        $statement = $Connector->prepare("SELECT * FROM :table_name");
         $statement->bindParam(":table_name",$reformatTableName);
-        $statement->bindParam(":array", $str);
-        $fetch = $statement->fetchAll(PDO::FETCH_COLUMN);
-        $count = $statement->rowCount();
+        $fetch = $statement->fetchAll(PDO::FETCH_ASSOC);
         $json = json_encode($fetch);
 
         return $json;
