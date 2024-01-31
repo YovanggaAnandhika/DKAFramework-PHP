5741ee79894a23ea66397aff8151ff01537304d8
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-01-31 22:06:32 +0800

    Add Test Files

--- test/index.php
+++ test/index.php
@@ -1,18 +1,21 @@
 <?php
 
-use yovanggaanandhika\dkaframework\Module\Database\Connector as Connect;
-use yovanggaanandhika\dkaframework\Module\Database\CRUD as CRUD;
+require_once("./../vendor/autoload.php");
+
+use yovanggaanandhika\dkaframework\Module\Database\CRUD\Read\OptionsRead as Options;
+use yovanggaanandhika\dkaframework\Module\Database\MariaDB as MariaDB;
 
 // Run Code
-
     $db_config = array(
         'host' => "localhost",
-        'user' => "root",
-        'password' => "",
-        'database' => "test"
+        'user' => "developer",
+        'password' => "Cyberhack2010",
+        'database' => "dka"
     );
-    $mConnect = Connect::MariaDB($db_config);
-    $Read = CRUD::Read($mConnect, "test", true);
+    $MariaDB = new MariaDB($db_config);
+    $CRUD = $MariaDB->CRUD();
+    $Options = new Options();
+    $Read = $CRUD->Read("mti-bigdata-data-lokasi_provinsi", $Options->limit(1)->jsonFormat(true));
     print_r($Read);
 
 
